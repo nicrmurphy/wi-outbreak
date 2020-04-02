f5885155965188903bc2987f228f01ac460cd7d9
Nic Murphy <nicrmurphy@live.com>
2020-04-02 10:35:59 -0500

    adjust runanble.js

--- src/runnable.js
+++ src/runnable.js
@@ -8,9 +8,6 @@ function updateMapData(data) {
   // const averages = casesArray.map(c => Math.pow(c - averageCases, 2))
   // const stdDev = Math.sqrt(averages.reduce(sumArr) / averages.length)
 
-  const k = 1 - averageCases / maxCases // contour of the linear regression
-  const [max, min] = [255, 100]
-
   // reset map colors
   const mapdata = window.simplemaps_statemap.mapdata
   for (const county in mapdata.state_specific) {
@@ -22,6 +19,7 @@ function updateMapData(data) {
       GEOID: id,
       NAME: name,
       POSITIVE: cases,
+      NEGATIVE: negative,
       DEATHS: deaths,
       CMNTY_SPRD: cmntySprd,
       DATE: date,
@@ -30,27 +28,30 @@ function updateMapData(data) {
     // console.log(name, color.hex())
     let desc = ''
     desc = desc.concat(
-      `<strong>${cases} ${cases === 1 ? 'case' : 'cases'}</strong><br/>`
+      `<strong>${cases} ${
+        cases === 1 ? 'positive case' : 'positive cases'
+      }</strong><br/>`
     )
+    desc = negative
+      ? desc.concat(
+          `<strong>${negative} ${
+            negative === 1 ? 'negative case' : 'negative cases'
+          }</strong><br/>`
+        )
+      : desc
     desc = desc.concat(
       `<strong>${deaths} ${deaths === 1 ? 'death' : 'deaths'}</strong><br/>`
     )
-    desc = desc.concat(
-      `${cmntySprd ? '<strong>* Community Spread</strong><br/>' : ''}`
-    )
+    desc = cmntySprd
+      ? desc.concat('<strong>* Community Spread</strong><br/>')
+      : desc
     desc = desc.concat(`As of: ${date}<br/> ~2:00pm CST`)
     mapdata.state_specific[id].description = desc
-
-    const r =
-      ((max - min) * (Math.pow(k, cases - maxCases) - 1)) /
-        (Math.pow(k, 1 - maxCases) - 1) +
-      min
-
-    // mapdata.state_specific[id].color = `rgb(${r}, 40, 0)` // ferrari
-    // mapdata.state_specific[id].color = `rgb(${r}, 41, 57)` // imperial red
-    // mapdata.state_specific[id].color = `rgb(${r ? r : max}, 41, 32)`
-    mapdata.state_specific[id].color = color.hex()
-    mapdata.state_specific[id].hover_color = '#FF9000'
+    
+    if (cases > 0) {
+      mapdata.state_specific[id].color = color.hex()
+    }
+    mapdata.state_specific[id].hover_color = color.brighten().hex()
   }
   window.simplemaps_statemap.mapdata = mapdata
   if (window.simplemaps_statemap.refresh) {
